35e68c732be65f7cc16f5e40f23603f6afd59972
Sigfried Gold <sigfried@sigfried.org>
2016-11-27 11:29:26 -0500

    histogram is default now. got kde working, but it's too slow. lots of ways could make things faster

--- src/components/DistCharts.js
+++ src/components/DistCharts.js
@@ -143,9 +143,9 @@ export class DistSeries extends Component {
     super(props);
     this.state = { 
       useFullHeight: false,
-      DistChartType: 'DistBars',
+      DistChartType: 'Histogram',
       ChartTypes : {
-        DistBars, CumulativeDistFunc,
+        DistBars, CumulativeDistFunc, Histogram,
       }
     };
   }
@@ -200,6 +200,14 @@ export class DistSeries extends Component {
                 <div className="dist-series-title">
                   {title}
                 </div>
+                <Radio inline
+                  checked={DistChartType==='Histogram'}
+                  value={'Histogram'}
+                  onChange={this.onDistChartChange.bind(this)}
+                >
+                  Histogram
+                </Radio>
+                {' '}
                 <Radio inline 
                   checked={DistChartType==='DistBars'}
                   value={'DistBars'}
@@ -214,17 +222,6 @@ export class DistSeries extends Component {
                   onChange={this.onDistChartChange.bind(this)}
                 >
                   Cumulative Distribution Function
-                </Radio>
-                {' '}
-                <Radio inline
-                  title="blah blah blah"
-                  disabled={true}
-                  checked={DistChartType==='DensityEstimation'}
-                  //checked={true}
-                  value={'DensityEstimation'}
-                  onChange={this.onDistChartChange.bind(this)}
-                >
-                  Density Estimation
                 </Radio>
               </div>
               {Dists}
@@ -443,6 +440,8 @@ export class CumulativeDistFunc extends SmallChart {
       y.domain([0, distCnt]);
     }
                 
+    //d3.select(this.yAxisG).call(yAxis);
+
     const recs = dist.records.filter(d=>getX(d) !== null);
     let data = recs.map((d,i) => {
       return {
@@ -488,127 +487,119 @@ export class CumulativeDistFunc extends SmallChart {
   }
 }
 
-function KDE() {
-  // USER DEFINABLE VARIABLES START
-  var numHistBins = 10; // number of bins for the histogram
-  var calcHistBinsAutmoatic = true; // if true, the number of bins are calculated automatically and
-  // numHistBins is overwritten
-  var showKDP = true; // show the kernel density plot?
-  var bandwith = 4; // bandwith (smoothing constant) h of the kernel density estimator
-  var dataFN = "./faithful.json"; // the filename of the data to be visualized
+export class Histogram extends SmallChart {
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+    let {dist} = this.props;
+    super.componentDidMount(dist.records);
+  }
+  render() {
+    const {dist, allDists, distNum, getX, getY, 
+            useFullHeight, distCnt, maxCnt} = this.props;
+    const {lo, y, x, xAxis, yAxis} = this.state;
+    var numHistBins = 10; // number of bins for the histogram
+    var calcHistBinsAutmoatic = true; // if true, the number of bins are calculated automatically and
+    // numHistBins is overwritten
+    var showKDP = true; // show the kernel density plot?
+    var bandwith = 4; // bandwith (smoothing constant) h of the kernel density estimator
 
+    if (!x) return super.render('');
 
-  // USER DEFINABLE VARIABLES END
+    //const distCnt = dist.aggregate(_.sum, 'count');
+    //const maxCnt = allDists[0].aggregate(_.sum, 'count');
 
+    let numbers = _.flatten(dist.records.map(
+      d => {
+        return Array(d.count).fill(getX(d))
+      }));
+                
+    x.range([0, lo.chartWidth()])
+    y.range([lo.chartHeight(), 0])
+    //x.domain(d3.extent(data, function(d) { return d.x; })).nice();
+    //y.domain(d3.extent(data, function(d) { return d.y; })).nice();
 
-  var margin = {top: 20, right: 30, bottom: 30, left: 40},
-      width = 960 - margin.left - margin.right,
-      height = 500 - margin.top - margin.bottom;
+    //console.log(data.map(d=>`(${d.x},${d.y})`).join(' '));
+    //console.log(recs);
 
-  // the x-scale parameters
-  var x = d3.scale.linear()
-      .domain([30, 110])
-      .range([0, width]);
+    var histogram;
+    let svg = d3.select(this.chartG);
 
-  // the y-scale parameters
-  var y = d3.scale.linear()
-      .domain([0, .15])
-      .range([height, 0]);
-
-  var xAxis = d3.svg.axis()
-      .scale(x)
-      .orient("bottom");
-
-  var yAxis = d3.svg.axis()
-      .scale(y)
-      .orient("left")
-      .tickFormat(d3.format("%"));
-
-  var line = d3.svg.line()
-      .x(function(d) { return x(d[0]); })
-      .y(function(d) { return y(d[1]); });
-
-  // the histogram function
-  var histogram;
-
-  var svg = d3.select("body").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-  // draw the background
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
-    .append("text")
-      .attr("class", "label")
-      .attr("x", width)
-      .attr("y", -6)
-      .style("text-anchor", "end")
-      .text("Time between Eruptions (min.)");
-
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis);
-
-
-  // draw the histogram and kernel density plot  
-  d3.json(dataFN, function(error, faithful) {
-
-      // calculate the number of histogram bins
+    // calculate the number of histogram bins
     if( calcHistBinsAutmoatic == true) {
-      numHistBins = Math.ceil(Math.sqrt(faithful.length));  // global variable
+      numHistBins = Math.ceil(Math.sqrt(numbers.length));  // global variable
     }
-  // the histogram function
-    histogram = d3.layout.histogram()
-      .frequency(false)
-      .bins(numHistBins);
+    // the histogram function
+    histogram = d3.histogram()
+      //.thresholds(numHistBins);
     //.bins(x.ticks(500));
     
-    var data = histogram(faithful);
-    //var kde = kernelDensityEstimator(epanechnikovKernel(7), x.ticks(100));
-    var kde = kernelDensityEstimator(epanechnikovKernel(bandwith), x.ticks(100));
+    var data = histogram(numbers);
+
+    if (!useFullHeight) {
+      let exp1Numbers = _.flatten(allDists[0].lookup('duration').records.map(
+        d => {
+          return Array(d.count).fill(getX(d))
+        }));
+      y.domain([0, _.max(histogram(exp1Numbers).map(d=>d.length))]);
+    } else {
+      y.domain([0, distCnt]);
+      y.domain([0, _.max(data.map(d=>d.length))]);
+    }
+    x.domain(d3.extent(numbers));
+    d3.select(this.yAxisG).call(yAxis);
     
     //alert("kde is " + kde.toSource());
     
-    //console.log(svg.datum(kde(faithful)));
+    //console.log(svg.datum(kde(numbers)));
 
+    svg.selectAll(".bar").remove();
     svg.selectAll(".bar")
         .data(data)
       .enter().insert("rect", ".axis")
         .attr("class", "bar")
-        .attr("x", function(d) { return x(d.x) + 1; })
-        .attr("y", function(d) { return y(d.y); })
-        .attr("width", x(data[0].dx + data[0].x) - x(data[0].x) - 1)
-        .attr("height", function(d) { return height - y(d.y); });
+        .attr("x", function(d) { return x(d.x0) + 1; })
+        .attr("y", function(d) { return y(d.length); })
+        .attr("width", x(data[0].x1 - x(data[0].x0) - 1))
+        .attr("height", function(d) { return lo.chartHeight() - y(d.length); });
     
+    /* too slow...probably easy to make faster
+    //var kde = kernelDensityEstimator(epanechnikovKernel(7), x.ticks(100));
+    var kde = kernelDensityEstimator(epanechnikovKernel(2), x.ticks(40));
+    y.domain(d3.extent(kde(numbers), d=>d[1]));
+    
+    var line = d3.line()
+        .x(function(d) { return x(d[0]); })
+        .y(function(d) { return y(d[1]); });
+
     // show the kernel density plot
     if(showKDP == true) {
       svg.append("path")
-        .datum(kde(faithful))
+        .datum(kde(numbers))
         .attr("class", "line")
         .attr("d", line);
-      }
+    }
+    */
 
-  });
+    function kernelDensityEstimator(kernel, x) {
+      return function(sample) {
+        return x.map(function(x) {
+          //console.log(x + " ... " + d3.mean(sample, function(v) { return kernel(x - v); }));    
+          return [x, d3.mean(sample, function(v) { return kernel(x - v); })];
+        });
+      };
+    }
 
-  function kernelDensityEstimator(kernel, x) {
-    return function(sample) {
-      return x.map(function(x) {
-      //console.log(x + " ... " + d3.mean(sample, function(v) { return kernel(x - v); }));    
-      return [x, d3.mean(sample, function(v) { return kernel(x - v); })];
-      });
-    };
-  }
-
-  function epanechnikovKernel(bandwith) {
-    return function(u) {
-      //return Math.abs(u /= bandwith) <= 1 ? .75 * (1 - u * u) / bandwith : 0;
-    if(Math.abs(u = u /  bandwith) <= 1) {
-    return 0.75 * (1 - u * u) / bandwith;
-    } else return 0;
-    };
+    function epanechnikovKernel(bandwith) {
+      return function(u) {
+        //return Math.abs(u /= bandwith) <= 1 ? .75 * (1 - u * u) / bandwith : 0;
+        if (Math.abs(u = u /  bandwith) <= 1) {
+          return 0.75 * (1 - u * u) / bandwith;
+        } else 
+          return 0;
+      };
+    }
+    return super.render('');
   }
 }
